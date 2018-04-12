f87c6cbc9bd8d074ff5d1efbdbc93d2fe30ede71
KyleDSLU <kyle.deprow@slu.edu>
2018-04-11 23:03:56 -0500

    more work

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import csv
 import unicodecsv as csvu
 import random
+import unidecode
 
 def decode_csv(csv_file):
     i = 0
@@ -15,6 +16,8 @@ def decode_csv(csv_file):
                 values = [s.decode('utf-8') for s in temp]
 
             i = i+1
+
+            fin.close()
     return dict(zip(keys,values))
 
 def start_char_addition(file_name, end_char, start_char):
@@ -36,6 +39,10 @@ def start_char_addition(file_name, end_char, start_char):
                 par_writer = csv.writer(paramout)
                 par_writer.writerow([words,sentences])
 
+                paramout.close()
+                fout.close()
+                fin.close()
+
 def get_csvparameters(file_name):
     with open(file_name.split('.')[0]+'_param.csv', 'rb') as fin:
         reader = csv.reader(fin)
@@ -45,6 +52,7 @@ def get_csvparameters(file_name):
 
         keys = ['Words', 'Sentences']
         print param
+        fin.close()
         return dict(zip(keys, param))
 
 def split_train_dev(file_name, percent, start_char):
@@ -92,7 +100,33 @@ def split_train_dev(file_name, percent, start_char):
                     del train_data[j]
                 print('Training Data Formed')
 
+                '''Super ineffecient, TODO: Speed this up'''
                 for row in train_data:
                     trainwriter.writerow(row)
                 print('Training Data Saved')
+
+                devout.close()
+                trainout.close()
+                fin.close()
+    
+
+
+def format_answer(file_name, start_char):
+    with open(file_name, 'rb') as fin:
+        with open(file_name.split('.')[0]+'_fmt.csv', 'w+') as fout:
+            reader = csvu.reader(fin, encoding='utf-8')
+            writer = csvu.writer(fout, encoding='utf-8', quotechar = "'")
+
+            writer.writerow([u'id',u'token'])
+            for i,row in enumerate(reader):
+                if row[0] != start_char:
+                    writer.writerow([unicode(i),'\"'+row[0]+'\"'])
+        fout.close()
+    fin.close()
+
+def strip_dev(file_name):
+    with open(file_name, 'rb') as fin:
+        
+
+
 
