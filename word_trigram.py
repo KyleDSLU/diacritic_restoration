da5169f16bb6d8f7b3fe1e23634f8c58a0a84eee
KyleDSLU <deprowkj@slu.edu>
2018-05-06 13:47:32 -0500

    Pushing laptop

--- word_trigram.py
+++ word_trigram.py
@@ -4,6 +4,7 @@ import csv
 from itertools import zip_longest
 from datetime import datetime
 from collections import Counter
+import unicodecsv as csvu
 
 import pymp
 
@@ -24,17 +25,20 @@ def ngrams_split(lst, n):
     lst_out = []
     for i in range(len(lst)-n):
         if lst[i] not in word_to_ignore:
+            stop_ind = 0
             for j in range(1,n):
                 if (lst[i-j+1] in word_to_ignore):
                     stop_ind = j-1
                     break
                 else:
                     stop_ind = j
-
             string = lst[i]
             for j in range(1,stop_ind+1):
                 old_string = string
                 string = lst[i-j] + ' ' + string
+            lst_out.append(string)
+        elif lst[i] == u'<$>':
+            string = lst[i]
             lst_out.append(string)
 
     #return [' '.join(lst[i:i+n]) for i in range(len(lst)-n)]
@@ -42,34 +46,37 @@ def ngrams_split(lst, n):
 
 
 
-def three_gram_count(tokens, n_filter, n):
+def gram_count(tokens, n_filter, n):
     ngram_count = pymp.shared.list([])
     n_words = len(tokens)
     n_filter = n_filter
     n = n
     d = Counter(ngrams_split(tokens, n))
     lst = [[key,value] for key,value in d.items()]
-    length = len(lst)
     shared_token_list = pymp.shared.list(lst)
+    length = len(lst)
 
     lst = ngrams_split(tokens, n-1)
     shared_tokenng_list = pymp.shared.list(lst)
 
-    proc = 7
+    proc = 8
+    length = length/(proc-1) 
 
     with pymp.Parallel(proc) as p:
         i = 0
         for result in p.iterate(shared_token_list):
-            if i%1000==0:
-                print(i,length/proc,p.thread_num)
             i+=1
             count = result[1]
             ngram = result[0]
+            if i%1000==0:
+                print(i,length,p.thread_num)
             if count >= n_filter:
                 splitted_ngram = ngram.split()
+                #p.print(splitted_ngram)
                 ngram_freq = math.log(count/n_words, 10)
                 num = count*n_words
-                c2gram = shared_tokenng_list.count(splitted_ngram[0] + " " + splitted_ngram[1])
+                c2gram = shared_tokenng_list.count(" ".join(splitted_ngram[:-1]))
+
                 product = 1
                 for split in splitted_ngram:
                     product *= tokens.count(split)
@@ -82,15 +89,25 @@ def three_gram_count(tokens, n_filter, n):
 
 def n_grams_stat(input_file, encoding, n_filter, n):
     tokens = tokenize(input_file, encoding)
-    if n == 3:
-        return three_gram_count(tokens, n_filter, n)
+    if n:
+        return gram_count(tokens, n_filter, n)
     return []
 
 
 if __name__ == "__main__":
     start_time = datetime.now()
-    s = n_grams_stat("./data/azj_train.csv",'utf-8', n_filter=5, n=3)
-    for a, b, c, d, e in s:
-        print(a, b, c, d, e)
+    s = n_grams_stat("./data/azj_train.csv",'utf-8', n_filter=5, n=2)
+    dictionary = {}
+    for result in s:
+        dictionary[result[4]] = "{:10.6f}".format(result[2])
+
+    with open("./ref/bigram/bigram1p0f.csv", 'wb') as fout:
+        string = ','.join(dictionary.keys())+'\r\n'
+        print(type(string))
+        fout.write(string.encode('utf-8'))
+        string = ','.join(dictionary.values())+'\r\n'
+        fout.write(string.encode('utf-8'))
+        fout.close()
+ 
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
